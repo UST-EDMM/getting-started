7b8d26961d74ea7de706499c713f965561bc08f4
Michael Wurster <miwurster@googlemail.com>
2019-12-09 15:19:04 +0100

    Fix to configure database schema

--- icsoc-demo/files/mysql_database/configure.sh
+++ icsoc-demo/files/mysql_database/configure.sh
@@ -2,6 +2,7 @@
 if [[ -d "/docker-entrypoint-initdb.d" ]]; then
     find . -type f -name '*.sql' -exec cp {} /docker-entrypoint-initdb.d \;
 else
-    find . -type f -name '*.sql' | xargs | mysql -u root --password=${DBMS_ROOT_PASSWORD}
+    file_name=$(find . -type f -name '*.sql')
+    cat $file_name | mysql -u root --password=${DBMS_ROOT_PASSWORD}
 fi
 exit 0
